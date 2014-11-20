e5ffa1a38ce97ce5c8d12d517e3b073e6c0f0ac2
ORiGiNe <contact@equipe-origine.org>
2014-11-20 18:06:32 +0100

    test de ultrason pour crouzzz

--- JAVA_code/OrigineBrain/src/main/TestBidouille.java
+++ JAVA_code/OrigineBrain/src/main/TestBidouille.java
@@ -2,8 +2,10 @@ package main;
 
 import network.serial.ArduinoFinder;
 import network.serial.SerialConnection;
-import drivers.base.AttachInterruptOption;
+import drivers.base.InputPin;
+import drivers.base.OutputPin;
 import drivers.cardDrivers.ArduinoUnoCardDriver;
+import drivers.components.UltrasonDetector;
 
 public class TestBidouille {
 	public static void main(String[] args) {
@@ -11,14 +13,21 @@ public class TestBidouille {
 			SerialConnection conn = ArduinoFinder.getArduinoByName("arduino_uno_1");
 			ArduinoUnoCardDriver driver = new ArduinoUnoCardDriver(conn);
 			
-			driver.attachInterrupt(0, AttachInterruptOption.RISING);
+			//driver.attachInterrupt(0, AttachInterruptOption.RISING);
+			//System.out.println(driver.getAttachedCount(0));
 			
+			OutputPin trigger = driver.getOutputPin(12);
+			InputPin echo = driver.getInputPin(11);
+			
+			UltrasonDetector detector = new UltrasonDetector(trigger, echo);
+			
+			System.out.println("Begin");
 			while(true) {
-				System.out.println(driver.getAttachedCount(0));
-				Thread.sleep(1000);
+				System.out.println(detector.detect());
+				Thread.sleep(500);
 			}
-			/*
-			driver.closeConnection();
+			
+			/*driver.closeConnection();
 			ArduinoFinder.closeAllConnection();*/
 		} catch (Exception e) {
 			e.printStackTrace();
